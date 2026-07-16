34f9b0e0c55829bf8353fd0a829934a04aaf7124
tyroneblakestewart-dot <tyroneblakestewart@gmail.com>
2026-07-16 22:57:31 +0100

    Assemble embedded Hoodlums hero artwork

---
+++ lib/hoodlums-hero-image.ts
@@ -0,0 +1,6 @@
+import part0 from "./hoodlums-hero-image-0";
+import part1 from "./hoodlums-hero-image-1";
+import part2 from "./hoodlums-hero-image-2";
+import part3 from "./hoodlums-hero-image-3";
+
+export const HOODLUMS_HERO_IMAGE = `data:image/webp;base64,${part0}${part1}${part2}${part3}`;
