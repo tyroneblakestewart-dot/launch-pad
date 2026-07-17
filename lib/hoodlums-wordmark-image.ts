37f5b77c24cf0b2812448e6c4fdd7f64a6185f80
tyroneblakestewart-dot <tyroneblakestewart@gmail.com>
2026-07-17 11:17:55 +0100

    Fix broken Hoodlums wordmark asset
    * Add browser-served Hoodlums wordmark SVG
    
    * Serve Hoodlums wordmark from the public asset path

--- lib/hoodlums-wordmark-image.ts
+++ lib/hoodlums-wordmark-image.ts
@@ -1,7 +1,1 @@
-import { HOODLUMS_WORDMARK_IMAGE_0 } from "./hoodlums-wordmark-image-0";
-import { HOODLUMS_WORDMARK_IMAGE_1 } from "./hoodlums-wordmark-image-1";
-import { HOODLUMS_WORDMARK_IMAGE_2 } from "./hoodlums-wordmark-image-2";
-import { HOODLUMS_WORDMARK_IMAGE_3 } from "./hoodlums-wordmark-image-3";
-
-export const HOODLUMS_WORDMARK_IMAGE =
-  `data:image/png;base64,${HOODLUMS_WORDMARK_IMAGE_0}${HOODLUMS_WORDMARK_IMAGE_1}${HOODLUMS_WORDMARK_IMAGE_2}${HOODLUMS_WORDMARK_IMAGE_3}`;
+export const HOODLUMS_WORDMARK_IMAGE = "/hoodlums-wordmark.svg";
