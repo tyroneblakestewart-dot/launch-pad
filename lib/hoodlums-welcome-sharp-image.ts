550a489fd9294d9d577f2b731dfd43ee91bb9174
tyroneblakestewart-dot <tyroneblakestewart@gmail.com>
2026-07-20 14:04:19 +0100

    Assemble sharp welcome crew artwork

---
+++ lib/hoodlums-welcome-sharp-image.ts
@@ -0,0 +1,10 @@
+import { HOODLUMS_WELCOME_SHARP_PART_00 } from "./hoodlums-welcome-sharp/part-00";
+import {
+  HOODLUMS_WELCOME_SHARP_PART_01,
+  HOODLUMS_WELCOME_SHARP_PART_02,
+} from "./hoodlums-welcome-sharp/parts-01-02";
+
+export const HOODLUMS_WELCOME_SHARP_IMAGE =
+  `data:image/webp;base64,${HOODLUMS_WELCOME_SHARP_PART_00}` +
+  HOODLUMS_WELCOME_SHARP_PART_01 +
+  HOODLUMS_WELCOME_SHARP_PART_02;
